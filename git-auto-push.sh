bb237e4eb009f8cea067fc723334b7ea9688e478
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2021-10-27 18:24:15 -0500

    Update and rename git-automation.sh to git-auto-push.sh

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #A simple bash script that strings together the necessary commands to push to a repo.
+#Simple type ./git-auto-push followed by the message (no "")
 git add .
 git commit -m "$1"
 git push
