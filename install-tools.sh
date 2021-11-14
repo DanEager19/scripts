729b5e0398374511a26d49831bb3c7ef34e26095
Daniel Eager <daneager19@gmail.com>
2021-11-13 21:08:28 -0600

    Added tool installs

---
+++ install-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt install vim
+sudo apt install neovim
+
+sudo apt install docker.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+sudo apt install git
+
+sudo apt install tmux
+
+sudo apt install htop
