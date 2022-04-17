c5b0f321b8b518efbd6aeef08e01177ebd8ab131
Daniel Eager <daniel@deager.dev>
2022-04-16 23:14:55 -0500

    Slight tweaks

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt dist-upgrade
 
 #Install apps/tools from apt
-sudo apt-get install htop neovim ufw net-tools wget cron curl git grep tmux gcc g++ sed openssh
+sudo apt-get install htop docker.io neovim ufw net-tools wget cron curl git grep tmux gcc g++ sed openssh
 
 #Install Python
 sudo apt-get install python3 pip
@@ -17,23 +17,6 @@ nvm install --lts
 #Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env 
-
-#Install Docker Engine
-sudo apt-get update
-sudo apt-get install \ ca-certificates \ curl \ gnupg \ lsb-release
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
- sudo apt-get update
- sudo apt-get install -f docker-ce docker-ce-cli containerd.io
-
-#Install Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install code
 
 #Setup Git
 git config --global user.name "Daniel Eager"
