5c4f41a808d5fd5e955d98c8d575a39324576f4c
Daniel Eager <daniel@deager.dev>
2022-04-17 20:08:16 -0500

    Various additions

--- bitwarden.sh
+++ bitwarden.sh
@@ -2,9 +2,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker $1
+sudo apt install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#A simple bash script that strings together the necessary commands to push to a repo.
-#Simple type ./git-auto-push followed by the message (no "")
 git add .
 git commit -m "$1"
 git push
