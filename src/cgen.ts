a57fc6bf3c1b14cd458a1163640a1eeaf18087cd
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-08-15 14:05:16 -0500

    Cute cgen project

---
+++ src/cgen.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import { createSpinner } from 'nanospinner';
+import { outputFile } from 'fs-extra';
+
+const returnZero = `{
+        
+    return 0;
+}`;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function askName(): Promise<string> {
+    const answer = await inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'Give the project a name:',
+        default() {
+            return 'project';
+        }
+    });
+
+    return answer.name;
+}
+
+async function askHeaders(): Promise<string> {
+    const answer = await inquirer.prompt({
+        name: 'headers',
+        type: 'input',
+        message: 'Enter all header files to include. Separate header files with a space.',
+        default() {
+            return 'stdio.h';
+        },
+    });
+
+    return answer.headers;
+}
+
+async function askCmdArgs(): Promise<boolean> {
+    const answer = await inquirer.prompt({
+        name: 'cmdArgs',
+        type: 'confirm',
+        message: 'Will the program take command line arguments?',
+    });
+
+    return answer.cmdArgs;
+}
+
+async function generateBoilerplate(): Promise<void> {
+    let template = '';
+    const projectName = await askName();
+
+    const headerFiles = (await askHeaders()).split(' ');
+    const cmdArgs = await askCmdArgs();
+    const spinner = createSpinner().start();
+
+    headerFiles.forEach((header: any) => {
+        template += `#include <${header}>\n`;
+    });
+    
+    cmdArgs ? template += '\nint main(int argc, char *argv[])\n' : template += '\nint main()\n';
+    
+    template += returnZero
+    
+    if (fs.existsSync(`${projectName}/main.c`)) {
+        spinner.error({
+            text: chalk.redBright('[x] - Project already exists!')
+        });
+        process.exit(1);
+    } else {    
+        await outputFile(`${projectName}/main.c`, template, (e) => {
+            if (e) {
+                spinner.error({
+                    text: chalk.redBright(`[x] - ${e}`)
+                });
+                process.exit(1);
+            }
+        });
+    }    
+
+    await sleep(2000);
+
+    await spinner.success({
+        text: chalk.greenBright(`[+] - Success! C Project: ${[projectName]} generated!`)
+    });
+}
+
+console.clear()
+await generateBoilerplate()
