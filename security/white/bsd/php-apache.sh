b3ed3959669136fa96db28920b5433061128d43b
DanEager19 <81655709+DanEager19@users.noreply.github.com>
2022-12-20 17:47:59 -0600

    Why

---
+++ security/white/bsd/php-apache.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+su - root
+pkg update && pkg upgrade
+pkg install php80
+pkg install vim php80-xml mod_php80 php80-zip php80-mbstring php80-zlib php80-curl php80-mysqli php80-gd php80-gd
+pkg install apache24
+
+sysrc apache24_enable=yes
+service apache24 start
+tee /usr/local/www/apache24/data/index.php<<EOF
+<h1>See if a host is up!</h1>
+<form method="post">
+    <label for="host">Host IP:</label>
+    <input type="text" name="host" id="host">
+    <input type="submit" name="submit" value="ping">
+</form>
+<pre>
+    <?php 
+        if (isset($_POST['submit'])) {
+            $host = $_POST['host'];
+            $output = shell_exec("ping -c 4 $host");
+            echo($output);
+        }
+    ?>
+</pre>
+EOF
--- util/r2.py
+++ util/r2.py
@@ -6,39 +6,17 @@ import re
 
 class FunctionExam:
     def __init__ (self):
-        self.r2 = r2pipe.open("./OpenBTS.bin")
+        self.r2 = r2pipe.open("./p5.bin")
         self.r2.cmd("aaaa")
         self.functions = self.r2.cmd("aflj")
         self.jsonfunctions = json.loads(self.functions)
 
     def find_functions(self):
-        dangerousfunctions = [
-            "\.strcpy",
-            "\.strcat",
-            "\.strncpy",
-            "\.strncat",
-            "\.sprintf", 
-            "\.vsprintf", 
-            "\.scanf", 
-            "\.sscanf", 
-            "\.gets ", 
-            "\.atoi", 
-            "\.atof", 
-            "\.atol", 
-            "\.execve", 
-            "\.system", 
-            "\.popen"
-        ]
         print("[~] - Finding dangerous functions...")
         for jsonfunc in self.jsonfunctions:
-            for danger in dangerousfunctions:
-                if(re.search(danger, jsonfunc["name"]) != None):
-                    print("[~] - Call for {}".format(jsonfunc["name"]))
-                    for call in jsonfunc["codexrefs"]:
-                        addr = hex(call["addr"])
-                        funccall = self.r2.cmd("afd {}".format(addr)).split(' ')
-                        self.r2.cmd("s {}".format(hex(call["addr"])))
-                        print("\t[~] - Called @ {} by {}".format(addr, funccall[0]))
+            if(re.search("^function\_8.+aa$", jsonfunc["name"]) != None):
+                print("[~] - Call for {}".format(jsonfunc["name"]))
+                    
         print("[~] - Done!")
 
 f1 = FunctionExam()
