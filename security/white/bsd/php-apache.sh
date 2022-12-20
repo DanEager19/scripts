bd3c86ae3d1e22c0b1869d8c4ac92f0fb06ab5d9
DanEager19 <81655709+DanEager19@users.noreply.github.com>
2022-12-20 17:48:20 -0600

    modified install

--- security/white/bsd/php-apache.sh
+++ security/white/bsd/php-apache.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 su - root
 pkg update && pkg upgrade
-pkg install php80
-pkg install vim php80-xml mod_php80 php80-zip php80-mbstring php80-zlib php80-curl php80-mysqli php80-gd php80-gd
-pkg install apache24
+pkg install php80 vim php80-xml mod_php80 php80-zip php80-mbstring php80-zlib php80-curl php80-mysqli php80-gd php80-gd apache24
 
 sysrc apache24_enable=yes
 service apache24 start
