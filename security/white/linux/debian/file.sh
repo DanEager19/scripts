8c862bb3f3d504ec126d24cbd7498afdcf16fd46
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2023-02-09 17:04:20 -0600

    edited samba

---
+++ security/white/linux/debian/file.sh
@@ -0,0 +1,2 @@
+
+for d in $(ls /home); do echo "CCDC2023" > /home/$d/scoreme.txt ; done
--- security/white/linux/debian/samba.sh
+++ security/white/linux/debian/samba.sh
@@ -2,16 +2,21 @@
 
 apt update && apt install -y samba smbclient
 
-mkdir /tmp/sambashare
+mkdir /smbshare
 
 cat >> /etc/samba/smb.conf << EOF
-[sambashare]
+[global]
+public = yes
+map to guest = Bad User
+[share]
     comment = Samba Linux Server
-    path = /tmp/sambashare
+    path = /smbshare
     read only = no
+    writeable = yes
+    guest ok = yes
+    guest only = yes
+    public = yes    
     browsable = yes
 EOF
 
 systemctl enable smbd && systemctl start smbd
-
-smbpasswd -a $USER
