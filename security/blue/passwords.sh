ff6baec1be8cd03b2f9b5530c4d05b73428142ad
DanEager19 <81655709+DanEager19@users.noreply.github.com>
2023-03-01 21:16:57 -0600

    Update passwds

--- security/blue/passwords.sh
+++ security/blue/passwords.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Enter the new password:"; read; for u in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -d":" -f1); do echo "$u:$REPLY" | chpasswd -e ; done
+echo "Enter the new password:"; read; for u in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -d":" -f1); do echo "$u:$REPLY" | chpasswd ; done
 
 chown root:root /etc/passwd
 chmod 600 /etc/passwd
